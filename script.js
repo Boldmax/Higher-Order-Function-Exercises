74bfb884e9857f5c4928879ad88828eef244bf48
Boldmax <maxbold0007@gmail.com>
2020-08-04 05:47:45 +0100

    HOF Exercises

---
+++ script.js
@@ -0,0 +1,88 @@
+//      HIGHER ORDER FUNCTION EXERCISE
+
+   // Exercise 1
+   function countDown(time){
+    var timer = setInterval(function(){
+      time--;
+      if(time <= 0){
+        clearInterval(timer);
+        console.log('DONE!');
+      }
+      else {
+        console.log(time);
+      }
+  
+    },1000)
+  }
+
+  //  Exercise 2
+
+  function randomGame(){
+    var num;
+    var times = 0;
+    var timer = setInterval(function(){
+      num = Math.random();
+      times++
+      if(num > .75) {
+        clearInterval(timer);
+        console.log("It took " + times + " try/tries.");
+      }
+    },1000)
+  }
+
+  //   Exrecise 3
+
+  function isEven(num){
+    return num % 2 === 0;
+  }
+
+  //   Exercise 4
+  
+  function isOdd(num){
+    return num % 2 !== 0;
+  }
+  
+  //   Exercise 5
+  
+  function isPrime(value) {
+      for(var i = 2; i <= Math.sqrt(value); i++) {
+          if(value % i === 0) {
+              return false;
+          }
+      }
+      return value > 1;
+  }
+
+//   Exercise 6
+  
+  function numberFact(num, fn){
+    return fn(num);
+  }
+   
+//    Exercise 7
+
+  function find(arr, fn){
+    for(var i=0; i<arr.length; i++){
+      if(fn(arr[i])) return arr[i];
+    }
+  }
+  
+   //  Exercise 8
+
+  function findIndex(arr, fn){
+    for(var i=0; i<arr.length; i++){
+      if(fn(arr[i])) return i;
+    }
+  }
+  
+
+    //  Exercise 9
+
+  function specialMultiply(a,b){
+    if(arguments.length === 1){
+      return function(b){
+        return a*b;
+      }
+    }
+    return a*b;
+  }
